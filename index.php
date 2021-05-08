a2cbf0b6f80c3de49d0cbabf2357ab83033265b2
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-05-09 00:59:26 +0700

    Исправление структуры файла
    Изменен способ получения данных. Добавлены header-ы для сайта

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PUTCH,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 $method = $_SERVER['REQUEST_METHOD'];
 $formData = getFormData($method);
 $url = (isset($_GET['q'])) ? $_GET['q'] : '';
@@ -8,20 +15,30 @@ print_r($formData);
 ?>
 
 <?php
-function getFormData($method)
-{
-    // GET или POST: данные возвращаем как есть
+function getFormData($method) {    
     if ($method === 'GET') return $_GET;
-    if ($method === 'POST') return $_POST;
-    // PUT, PATCH или DELETE
+    if ($method === 'POST' && !empty($_POST)) return $_POST;
+
+    $incomingData = file_get_contents('php://input');
+    $decodedJSON = json_decode($incomingData); 
+    if ($decodedJSON) 
+    {
+        $data = $decodedJSON;
+    } 
+        else 
+    {
     $data = array();
-    $exploded = explode('&', file_get_contents('php://input'));
-    foreach ($exploded as $pair) {
-        $item = explode('=', $pair);
-        if (count($item) == 2) {
-            $data[urldecode($item[0])] = urldecode($item[1]);
-        }
+        $exploded = explode('&', file_get_contents('php://input'));     
+        foreach($exploded as $pair) 
+        {
+            $item = explode('=', $pair);
+            if (count($item) == 2) 
+            {
+                $data[urldecode($item[0])] = urldecode($item[1]);
+            }
+        } 
     }
     return $data;
 }
+
 ?>
