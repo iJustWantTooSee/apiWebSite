e7d3e9c9a828baecd66d87ea9cd910ad29576d56
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-05-09 03:27:50 +0700

    Добавлен контроллер для города
    Реализован метод Get.

---
+++ Controllers/cities.php
@@ -0,0 +1,55 @@
+<?php
+
+use DataBase\DatabaseConnector;
+
+require_once "DatabaseConnector.php";
+$db = new DatabaseConnector();
+function route($method, $urlData, $formData)
+{
+    switch ($method) {
+        case 'GET':
+            Get($method, $urlData, $formData);
+            break;
+        case 'POST':
+            break;
+        case 'PATCH':
+            break;
+        case 'DELETE':
+            break;
+    }
+}
+
+function Get($method, $urlData, $formData){
+    switch(sizeof($urlData)){
+        case 0:
+            OutputCities();
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        default:
+        //TODO сделать обработку ошибок
+            break;
+    }
+}
+
+function Post($method, $urlData, $formData){
+
+}
+
+function Patch($method, $urlData, $formData){
+
+}
+
+function Delete($method, $urlData, $formData){
+
+}
+
+function OutputCities(){
+    global $db;
+    $cities=$db->GetResultsQueries("SELECT * FROM city");
+    print_r($cities);
+}
+
+?>
