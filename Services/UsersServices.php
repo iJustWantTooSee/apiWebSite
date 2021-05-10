d1e75dd6f436684032a8ecd2d60d44ed21cff8dd
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-05-10 23:07:41 +0700

    Исправил создание юзера
    Теперь дата рождения вводится корректно. В дальнейшем будет рефакторинг

--- Services/UsersServices.php
+++ Services/UsersServices.php
@@ -4,7 +4,7 @@
     $db = new DatabaseConnector();
     
     class UsersServices{
-        function AddUser($method, $urlData, $formData){
+        function AddUser($method, $urlData, $formData) : bool{
             global $db;
             $MySqlLink= $db->GetMySqlLink();
             $Name = htmlentities(mysqli_real_escape_string($MySqlLink,$formData["Name"]));  
@@ -12,18 +12,24 @@
 	        $Username = htmlentities(mysqli_real_escape_string($MySqlLink,$formData["Username"]));  
 	        $Password = md5(htmlentities(mysqli_real_escape_string($MySqlLink,$formData["Password"])));  
 	        $Birthday = htmlentities(mysqli_real_escape_string($MySqlLink,$formData["Birthday"]));
-            if(!isset($Birthday)){
-                $Birthday = null;
-            }
+
             if($this->IsExistenceUser($Username)){
                 exit("Данный юзер уже существует");
             }
-            $query = "INSERT INTO `users`
+            if(!isset($Birthday) or $Birthday == ""){
+                $query = "INSERT INTO `users`
             (`Id`, `Name`, `Surname`, `UserName`, `Birthday`, `Avatar`, `Status`, `CityId`, `RoleId`, `Password`)
-             VALUES (null,'$Name','$Surname','$Username','$Birthday',null,null,null,null,'$Password')";
+             VALUES (null,'$Name','$Surname','$Username',null,null,null,null,null,'$Password')";
+            }
+            else{
+                $query = "INSERT INTO `users`
+                (`Id`, `Name`, `Surname`, `UserName`, `Birthday`, `Avatar`, `Status`, `CityId`, `RoleId`, `Password`)
+                 VALUES (null,'$Name','$Surname','$Username','$Birthday',null,null,null,null,'$Password')";
+            }
+
             if(!$db->Insert($query))
-                echo 'Произошла ошибка';
-            
+                return false;
+            return true;
 
         }
 
