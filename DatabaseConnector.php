93b3f1bb903d9f5c4a18ac9b9ba1eb29cca195df
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-05-12 16:26:24 +0700

    Добавил функцию возврата пользователя
    Данная функция может вернуть ассоциативный массив, не скрывая поля.

--- DatabaseConnector.php
+++ DatabaseConnector.php
@@ -67,18 +67,43 @@ class DatabaseConnector
             for ($i = 0; $i < $amount_rows; $i++) {
                 $temp_data = mysqli_fetch_assoc($res);
                 foreach ($temp_data as $key => $value) {
-                    if($_SESSION['role'] == "admin" and $key == "Birthday"){
+                    if ($_SESSION['role'] == "admin" and $key == "Birthday") {
                         $dataFromRow[$key] = $value;
-                    } else{
-                        if($_SESSION['role'] == "admin" and $key == "RoleId"){
-                        $request = "SELECT Name FROM `roles` WHERE Id =" . $value;
-                        $newValue = $this->GetResultsQueries($request);
-                        $dataFromRow[$key] = $newValue[0][0];
+                    } else {
+                        if ($_SESSION['role'] == "admin" and $key == "RoleId") {
+                            $request = "SELECT Name FROM `roles` WHERE Id =" . $value;
+                            $newValue = $this->GetResultsQueries($request);
+                            $dataFromRow[$key] = $newValue[0][0];
                         }
                     }
                     if ($key != "RoleId" and $key != "Birthday") {
-                        $dataFromRow[$key] = $value;  
-                    } 
+                        $dataFromRow[$key] = $value;
+                    }
+                }
+                $data[] = $dataFromRow;
+                $dataFromRow = array();
+            }
+            $link->close();
+            return $data;
+        }
+        $link->close();
+    }
+
+    function GetUserInfoForAdminAndUser($request): array
+    {
+        $link = $this->GetMySqlLink();
+        $res = mysqli_query($link, $request) or die("Ошибка " . mysqli_error($link));
+        if (!$res) //SQL
+        {
+            echo "Не удалось выполнить запрос: (" . $link->errno . ") " . $link->error;
+        } else {
+            $amount_rows = mysqli_num_rows($res);
+            $data = array();
+            $dataFromRow = array();
+            for ($i = 0; $i < $amount_rows; $i++) {
+                $temp_data = mysqli_fetch_assoc($res);
+                foreach ($temp_data as $key => $value) {
+                    $dataFromRow[$key] = $value;
                 }
                 $data[] = $dataFromRow;
                 $dataFromRow = array();
